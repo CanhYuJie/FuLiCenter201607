77c0ec6f55e87e5cc3380ec4e0f0cfbc42a6af34
[CanhYuJie] <[980804224@qq.com]>
2016-10-13 17:14:52 +0800

    modify Utils

--- app/src/main/java/com/yujie/fulicenter201607/widget/CatChildFilterButton.java
+++ app/src/main/java/com/yujie/fulicenter201607/widget/CatChildFilterButton.java
@@ -17,14 +17,12 @@ import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.yujie.fulicenter201607.I;
+import com.yujie.fulicenter201607.R;
+import com.yujie.fulicenter201607.utils.ConvertUtils;
+import com.yujie.fulicenter201607.utils.ImageLoader;
 
-import cn.ucai.fulicenter.I;
-import cn.ucai.fulicenter.R;
-import cn.ucai.fulicenter.activity.CategoryChildActivity;
-import cn.ucai.fulicenter.bean.CategoryChildBean;
-import cn.ucai.fulicenter.utils.ImageLoader;
-import cn.ucai.fulicenter.utils.Utils;
+import java.util.ArrayList;
 
 /**
  * 显示分类中当前所属小类的列表
@@ -36,7 +34,7 @@ public class CatChildFilterButton extends Button {
     CatChildFilterButton mbtnTop;
     PopupWindow mPopupWindow;
     GridView mgvCategory;
-    CatFilterAdapter mAdapter;
+//    CatFilterAdapter mAdapter;
     OnClickListener mListener;
 
     /**
@@ -59,7 +57,7 @@ public class CatChildFilterButton extends Button {
         if(mgvCategory.getAdapter().getCount()<16){
             mPopupWindow.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
         }else{
-            mPopupWindow.setHeight(Utils.px2dp(mContext, 200));
+            mPopupWindow.setHeight(ConvertUtils.px2dp(mContext, 200));
         }
         mPopupWindow.setTouchable(true);
         mPopupWindow.setOutsideTouchable(true);
@@ -70,125 +68,125 @@ public class CatChildFilterButton extends Button {
 
     private void initGridView() {
         mgvCategory=new GridView(mContext);
-        mgvCategory.setColumnWidth(Utils.px2dp(mContext, 1500));
-        mgvCategory.setHorizontalSpacing(Utils.px2dp(mContext, 10));
-        mgvCategory.setVerticalSpacing(Utils.px2dp(mContext, 10));
+        mgvCategory.setColumnWidth(ConvertUtils.px2dp(mContext, 1500));
+        mgvCategory.setHorizontalSpacing(ConvertUtils.px2dp(mContext, 10));
+        mgvCategory.setVerticalSpacing(ConvertUtils.px2dp(mContext, 10));
         mgvCategory.setNumColumns(GridView.AUTO_FIT);
         mgvCategory.setBackgroundColor(Color.TRANSPARENT);
         mgvCategory.setPadding(3, 3, 3, 3);
         mgvCategory.setCacheColorHint(0);
     }
-
-    private void setBtnTopArrow() {
-        Drawable right=null;
-        if(mExpandOff){
-            right=mContext.getResources().getDrawable(R.drawable.arrow2_down);
-        }else{
-            right=mContext.getResources().getDrawable(R.drawable.arrow2_up);
-        }
-        right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
-        mbtnTop.setCompoundDrawablesWithIntrinsicBounds(null, null, right, null);
-        mExpandOff=!mExpandOff;
-    }
-
-    /**
-     * 显示分类列表的适配器
-     * @author yao
-     *
-     */
-    class CatFilterAdapter extends BaseAdapter {
-        Context context;
-        ArrayList<CategoryChildBean> Children;
-
-        public CatFilterAdapter(Context context,
-                                ArrayList<CategoryChildBean> list) {
-            super();
-            this.context = context;
-            this.Children = list;
-        }
-
-        @Override
-        public int getCount() {
-            return Children==null?0:Children.size();
-        }
-
-        @Override
-        public CategoryChildBean getItem(int position) {
-            return Children.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View layout, final ViewGroup parent) {
-            ViewChildHolder holder=null;
-            if(layout==null){
-                layout= View.inflate(context, R.layout.item_cat_filter, null);
-                holder=new ViewChildHolder();
-                holder.layoutItem=(RelativeLayout) layout.findViewById(R.id.layout_category_child);
-                holder.ivThumb=(ImageView) layout.findViewById(R.id.ivCategoryChildThumb);
-                holder.tvChildName=(TextView) layout.findViewById(R.id.tvCategoryChildName);
-                layout.setTag(holder);
-            }else{
-                holder=(ViewChildHolder) layout.getTag();
-            }
-            final CategoryChildBean child =getItem(position);
-            String name=child.getName();
-            holder.tvChildName.setText(name);
-            String imgUrl=child.getImageUrl();
-            ImageLoader.downloadImg(context,holder.ivThumb,imgUrl);
-
-            holder.layoutItem.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(mPopupWindow.isShowing()){
-                        mPopupWindow.dismiss();
-                    }
-                    Intent intent=new Intent(mContext, CategoryChildActivity.class);
-                    intent.putExtra(I.CategoryChild.CAT_ID, child.getId());
-                    intent.putExtra("childList", Children);
-                    intent.putExtra(I.CategoryGroup.NAME, mbtnTop.getText().toString());
-                    mContext.startActivity(intent);
-                    ((CategoryChildActivity)mContext).finish();
-                }
-            });
-            return layout;
-        }
-
-        class ViewChildHolder{
-            RelativeLayout layoutItem;
-            ImageView ivThumb;
-            TextView tvChildName;
-        }
-    }
-
-    /**
-     * 设置分类列表的下拉按钮单击事件监听
-     * @param groupName
-     * @param childList
-     */
-    public void setOnCatFilterClickListener(final String groupName,
-                                            final ArrayList<CategoryChildBean> childList){
-        mbtnTop.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mbtnTop.setTextColor(Color.WHITE);
-                mbtnTop.setText(groupName);
-                if(mExpandOff){//若分类列表的窗口未打开，则弹出窗口
-                    mAdapter=new CatFilterAdapter(mContext, childList);
-                    mgvCategory.setAdapter(mAdapter);
-                    initPopupWindow();
-                }else{//否则，关闭窗口
-                    if(mPopupWindow.isShowing()){
-                        mPopupWindow.dismiss();
-                    }
-                }
-                setBtnTopArrow();
-            }
-        });
-    }
+//
+//    private void setBtnTopArrow() {
+//        Drawable right=null;
+//        if(mExpandOff){
+//            right=mContext.getResources().getDrawable(R.drawable.arrow2_down);
+//        }else{
+//            right=mContext.getResources().getDrawable(R.drawable.arrow2_up);
+//        }
+//        right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+//        mbtnTop.setCompoundDrawablesWithIntrinsicBounds(null, null, right, null);
+//        mExpandOff=!mExpandOff;
+//    }
+//
+//    /**
+//     * 显示分类列表的适配器
+//     * @author yao
+//     *
+//     */
+//    class CatFilterAdapter extends BaseAdapter {
+//        Context context;
+//        ArrayList<CategoryChildBean> Children;
+//
+//        public CatFilterAdapter(Context context,
+//                                ArrayList<CategoryChildBean> list) {
+//            super();
+//            this.context = context;
+//            this.Children = list;
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return Children==null?0:Children.size();
+//        }
+//
+//        @Override
+//        public CategoryChildBean getItem(int position) {
+//            return Children.get(position);
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            // TODO Auto-generated method stub
+//            return 0;
+//        }
+//
+//        @Override
+//        public View getView(int position, View layout, final ViewGroup parent) {
+//            ViewChildHolder holder=null;
+//            if(layout==null){
+//                layout= View.inflate(context, R.layout.item_cat_filter, null);
+//                holder=new ViewChildHolder();
+//                holder.layoutItem=(RelativeLayout) layout.findViewById(R.id.layout_category_child);
+//                holder.ivThumb=(ImageView) layout.findViewById(R.id.ivCategoryChildThumb);
+//                holder.tvChildName=(TextView) layout.findViewById(R.id.tvCategoryChildName);
+//                layout.setTag(holder);
+//            }else{
+//                holder=(ViewChildHolder) layout.getTag();
+//            }
+//            final CategoryChildBean child =getItem(position);
+//            String name=child.getName();
+//            holder.tvChildName.setText(name);
+//            String imgUrl=child.getImageUrl();
+//            ImageLoader.downloadImg(context,holder.ivThumb,imgUrl);
+//
+//            holder.layoutItem.setOnClickListener(new OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if(mPopupWindow.isShowing()){
+//                        mPopupWindow.dismiss();
+//                    }
+//                    Intent intent=new Intent(mContext, CategoryChildActivity.class);
+//                    intent.putExtra(I.CategoryChild.CAT_ID, child.getId());
+//                    intent.putExtra("childList", Children);
+//                    intent.putExtra(I.CategoryGroup.NAME, mbtnTop.getText().toString());
+//                    mContext.startActivity(intent);
+//                    ((CategoryChildActivity)mContext).finish();
+//                }
+//            });
+//            return layout;
+//        }
+//
+//        class ViewChildHolder{
+//            RelativeLayout layoutItem;
+//            ImageView ivThumb;
+//            TextView tvChildName;
+//        }
+//    }
+//
+//    /**
+//     * 设置分类列表的下拉按钮单击事件监听
+//     * @param groupName
+//     * @param childList
+//     */
+//    public void setOnCatFilterClickListener(final String groupName,
+//                                            final ArrayList<CategoryChildBean> childList){
+//        mbtnTop.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mbtnTop.setTextColor(Color.WHITE);
+//                mbtnTop.setText(groupName);
+//                if(mExpandOff){//若分类列表的窗口未打开，则弹出窗口
+//                    mAdapter=new CatFilterAdapter(mContext, childList);
+//                    mgvCategory.setAdapter(mAdapter);
+//                    initPopupWindow();
+//                }else{//否则，关闭窗口
+//                    if(mPopupWindow.isShowing()){
+//                        mPopupWindow.dismiss();
+//                    }
+//                }
+//                setBtnTopArrow();
+//            }
+//        });
+//    }
 }
