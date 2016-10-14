3d6dbe25c4dd6dec41619fec012b2c840e175649
[CanhYuJie] <[980804224@qq.com]>
2016-10-14 11:04:40 +0800

    added the alphaAnimation resource,added animation for SplashActiity,added logic to go MainActivity

--- app/src/main/java/com/yujie/fulicenter201607/view/activity/MainActivity.java
+++ app/src/main/java/com/yujie/fulicenter201607/view/activity/MainActivity.java
@@ -1,12 +1,14 @@
 package com.yujie.fulicenter201607.view.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yujie.fulicenter201607.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Context mContext = MainActivity.this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/yujie/fulicenter201607/view/activity/SplashActivity.java
+++ app/src/main/java/com/yujie/fulicenter201607/view/activity/SplashActivity.java
@@ -1,15 +1,53 @@
 package com.yujie.fulicenter201607.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 import com.yujie.fulicenter201607.R;
+import com.yujie.fulicenter201607.utils.MFGT;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+    public static final String TAG = SplashActivity.class.getSimpleName();
+    @Bind(R.id.activity_splash)
+    RelativeLayout activitySplash;
+    private Context mContext = SplashActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        Animation alphaAnimation = AnimationUtils.loadAnimation(mContext, R.anim.splash_anim);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(mContext,MainActivity.class));
+                MFGT.finish(SplashActivity.this);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        activitySplash.startAnimation(alphaAnimation);
     }
 }
