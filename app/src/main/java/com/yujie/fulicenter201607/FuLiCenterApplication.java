6b02bb5fd3baef516ec75ab8f54ba084b738e549
[CanhYuJie] <[980804224@qq.com]>
2016-10-14 09:47:49 +0800

    in OkHttpUtils.java,repair the error (Application.Context),repair Application file

--- app/src/main/java/com/yujie/fulicenter201607/FuLiCenterApplication.java
+++ app/src/main/java/com/yujie/fulicenter201607/FuLiCenterApplication.java
@@ -7,9 +7,11 @@ import android.content.Context;
  * Created by yujie on 16-10-13.
  */
 
-public class FuLiCenterApplication extends Application {
+public  class FuLiCenterApplication extends Application {
     public static final String TAG = FuLiCenterApplication.class.getSimpleName();
     private static FuLiCenterApplication instance = null;
+    public static Context applicationContext = getInstance();
+
     private FuLiCenterApplication(){}
     public static FuLiCenterApplication getInstance() {
         synchronized (FuLiCenterApplication.class) {
